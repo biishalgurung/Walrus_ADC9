52e15bfec29afc2804c6955e7fc8287669b6cea9
PRAJWAL <prajwalpandey45@gmail.com>
2020-01-08 08:37:23 +0545

    dfff

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .views import (searchposts)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+   url(r'^$', searchposts, name='searchposts'),
 ]
